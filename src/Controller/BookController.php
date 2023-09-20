cb07dee171481867612c156dc59c5dc66fdc21bd
Issam KHADIRI <khadiri.issam@gmail.com>
2023-09-20 21:53:59 +0100

    [feature] linking pages

---
+++ src/Controller/BookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/books', name: 'app_book_')]
+class BookController extends AbstractController
+{
+    #[Route('/', name: 'list')]
+    public function index(): Response
+    {
+        return $this->render('book/index.html.twig', [
+            'controller_name' => 'BookController',
+        ]);
+    }
+}
