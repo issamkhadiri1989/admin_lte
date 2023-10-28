8dbf047fb2db53985aeaa1a5f0addfd7c994d73a
Issam KHADIRI <khadiri.issam@gmail.com>
2023-10-28 11:13:38 +0100

    seance doctrine

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,27 +4,39 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Entity\Book;
 use App\Factory\AuthorFactory;
 use App\Factory\BookFactory;
 use App\Factory\CategoryFactory;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route(path: "/", name: "app_default")]
-    public function index(): Response
+    public function index(EntityManagerInterface $manager, Request $request): Response
     {
-        $categories = CategoryFactory::createMany(10);
+        $repository = $manager->getRepository(Book::class);
+        $page = $request->query->get('page');
 
-        $authors = AuthorFactory::createMany(5);
+        $books = $repository->findBy(
+            criteria: [],
+            orderBy: ['id' => 'asc'],
+            limit: 10,
+            offset: ($page - 1) * 10
+        );
+//        $books = $repository->findByIsbn('9790488437566');
 
-        $books = BookFactory::createMany(500, [
-            'author' => AuthorFactory::random(),
-            'categories' => CategoryFactory::randomRange(1, 4),
-        ]);
+        $book = $repository->findOneBy(['isbn' => '9790488437566']);
 
-        return $this->render('default/index.html.twig');
+        return $this->render(
+            'default/index.html.twig',
+            [
+                'booksList' => $books,
+            ]
+        );
     }
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Factory\AuthorFactory;
+use App\Factory\BookFactory;
+use App\Factory\CategoryFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $categories = CategoryFactory::createMany(10);
+
+        $authors = AuthorFactory::createMany(5);
+
+        $books = BookFactory::createMany(500, [
+            'author' => AuthorFactory::random(),
+            'categories' => CategoryFactory::randomRange(1, 4),
+        ]);
+
+        $manager->flush();
+    }
+}
